dd0c5bc40350411396458733da8c6418c6c9ec0e
Aseel BaniFadel <aseel.b.banifadel@gmail.com>
2020-11-21 19:38:28 +0200

    Catalog Server File

---
+++ Catalog Server.java
@@ -0,0 +1,26 @@
+import static spark.Spark.*;
+public class CatalogServer
+{
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		port(456);
+		Gson g = new Gson();
+    	get("Lookup/:id",( req,res)->{int ID = Integer.parseInt(req.params(":id"));
+    	String path = "/home/aseel/Desktop/SharedFolders/books.txt";
+    		return gson.toJson(CSVReaderInJava.LookUp(path, ID));			
+    	});
+    	
+    	get("Search/:topic",( req,res)->{String topic = req.params(":topic");
+    	String path = "/home/aseel/Desktop/SharedFolders/books.txt";
+    		return gson.toJson(CSVReaderInJava.SearchByTopic(path, topic));			
+    	});
+
+	}
+	
+	
+	
+	
+}
