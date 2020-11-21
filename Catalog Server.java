599724689ae8a1ff0ce4250136c094bc19e69324
Aseel BaniFadel <aseel.b.banifadel@gmail.com>
2020-11-21 20:19:37 +0200

    Update Catalog Server.java

--- Catalog Server.java
+++ Catalog Server.java
@@ -6,7 +6,7 @@ public class CatalogServer
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
-		port(456);
+		port(4567);
 		Gson g = new Gson();
     	get("Lookup/:id",( req,res)->{int ID = Integer.parseInt(req.params(":id"));
     	String path = "/home/aseel/Desktop/SharedFolders/books.txt";
