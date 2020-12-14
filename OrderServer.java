f5ecca80239e83150122024ad42d77a949715d21
Aseel BaniFadel <aseel.b.banifadel@gmail.com>
2020-12-14 22:39:29 +0200

    For Lab2

--- OrderServer.java
+++ OrderServer.java
@@ -9,9 +9,9 @@ public class OrderServer
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Gson g = new Gson();
-		port(6543);
+		port(7458);
     	get("Buy/:id",( req,res)->{int ID = Integer.parseInt(req.params(":id"));
-    	String path = "/home/aseel/Desktop/SharedFolders/books.txt";
+    	String path = "D:/Documents/Desktop/Eng/Books.txt";
     		return g.toJson(CSVReaderInJava.Buy(path, ID));			
     	});
 
