f08b913238150b019a014938bee18d923df59632
Aseel BaniFadel <aseel.b.banifadel@gmail.com>
2020-12-15 00:34:05 +0200

    After round robin

--- Catalog Server.java
+++ Catalog Server.java
@@ -1,5 +1,8 @@
 import static spark.Spark.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.gson.Gson;
 public class CatalogServer
 {
@@ -7,11 +10,13 @@ public class CatalogServer
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
-		port(9638);
+		
+		
+		port(Integer.parseInt(args[0]));
 		Gson g = new Gson();
     	get("Lookup/:id",( req,res)->{int ID = Integer.parseInt(req.params(":id"));
     	String path = "D:/Documents/Desktop/Eng/Books.txt";
+    	System.out.println(g.toJson(CSVReaderInJava.LookUp(path, ID)));
     		return g.toJson(CSVReaderInJava.LookUp(path, ID));			
     	});
     	
