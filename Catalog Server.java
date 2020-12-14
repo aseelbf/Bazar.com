f4eb4d9b823c6c804916f9aee46752ee4e9dd7d9
Aseel BaniFadel <aseel.b.banifadel@gmail.com>
2020-12-14 22:38:39 +0200

    For Lab2

--- Catalog Server.java
+++ Catalog Server.java
@@ -1,4 +1,6 @@
 import static spark.Spark.*;
+
+import com.google.gson.Gson;
 public class CatalogServer
 {
 
@@ -6,15 +8,15 @@ public class CatalogServer
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
-		port(4567);
+		port(9638);
 		Gson g = new Gson();
     	get("Lookup/:id",( req,res)->{int ID = Integer.parseInt(req.params(":id"));
-    	String path = "/home/aseel/Desktop/SharedFolders/books.txt";
+    	String path = "D:/Documents/Desktop/Eng/Books.txt";
     		return g.toJson(CSVReaderInJava.LookUp(path, ID));			
     	});
     	
     	get("Search/:topic",( req,res)->{String topic = req.params(":topic");
-    	String path = "/home/aseel/Desktop/SharedFolders/books.txt";
+    	String path = "D:/Documents/Desktop/Eng/Books.txt";
     		return g.toJson(CSVReaderInJava.SearchByTopic(path, topic));			
     	});
 
